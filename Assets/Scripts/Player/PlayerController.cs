9c6e5fafe164aa45b73ad302f785f5cbcd7c15c8
ZomDev3343 <contact.lefourretout@gmail.com>
2018-08-15 14:59:01 +0200

    First Commit - Initialization

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+	public float moveSpeed;
+	public float maxSpeed;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void CheckMovements()
+	{
+
+		bool horizontal = Input.GetButton("Horizontal");
+		bool vertical = Input.GetButton("Vertical");
+
+		float horizontalFactor = Input.GetAxisRaw("Horizontal");
+		float verticalFactor = Input.GetAxisRaw("Vertical");
+
+		if (horizontal)
+		{
+			
+		}
+		
+		
+
+	}
+}
